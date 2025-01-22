a56d84cc5e5e6538b990d394368e7504e64f1e71
Jean-Francois Vo <jeanfrancoisvo7@gmail.com>
2025-01-22 13:40:54 +0100

    🧱 Base path config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/xuan-and-hieu.github.io/',
   plugins: [
     vue(),
     vueJsx(),
