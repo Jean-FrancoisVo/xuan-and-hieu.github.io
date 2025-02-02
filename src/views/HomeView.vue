5cfcd234628e41084bd7d8ac55a1fd6bfe9231c9
Jean-Francois Vo <jeanfrancoisvo7@gmail.com>
2025-02-02 10:18:22 +0100

    ✨ Menu yay

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,9 +11,13 @@ const hours = untilWedding.getHours()
 const minutes = untilWedding.getMinutes()
 
 const isScrolled = ref(false)
+const openMenu = ref(false)
 
 const handleScroll = () => {
   isScrolled.value = window.scrollY > 10
+}
+const handleMenu = () => {
+  openMenu.value = !openMenu.value
 }
 
 onMounted(() => {
@@ -27,16 +31,15 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <header :class="{ scrolled: isScrolled }">
+  <header :class="{scrolled: isScrolled}">
     <div class="title">
       Xuan & Hieu
     </div>
-    <div class="menu">
-      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="" viewBox="0 8 50 47" fill="none">
-        <line x1="11.5" y1="16.5" x2="40.5" y2="16.5" stroke="#043927" stroke-width="3" stroke-linecap="round" />
-        <line x1="11.5" y1="26.5" x2="40.5" y2="26.5" stroke="#043927" stroke-width="3" stroke-linecap="round" />
-        <line x1="11.5" y1="36.5" x2="40.5" y2="36.5" stroke="#043927" stroke-width="3" stroke-linecap="round" />
-      </svg>
+    <div class="menu" @click="handleMenu" :class="{open: openMenu}">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
   </header>
   <main>
@@ -179,6 +182,60 @@ header.scrolled::before {
   opacity: 1;
 }
 
+.menu {
+  height: 4vh;
+  width: 12vw;
+  position: relative;
+  transform: rotate(0deg);
+  transition: .5s ease-in-out;
+  cursor: pointer;
+}
+
+.menu span {
+  display: block;
+  position: absolute;
+  height: 0.35vh;
+  width: 100%;
+  background: var(--color-sacramento);
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  transform: rotate(0deg);
+  transition: .25s ease-in-out;
+}
+
+.menu span:nth-child(1) {
+  top: 0;
+}
+
+.menu span:nth-child(2), .menu span:nth-child(3) {
+  top: 1.5vh;
+}
+
+.menu span:nth-child(4) {
+  top: 3vh;
+}
+
+.menu.open span:nth-child(1) {
+  top: 1.5vh;
+  width: 0;
+  left: 50%;
+}
+
+.menu.open span:nth-child(2) {
+  transform: rotate(45deg);
+}
+
+.menu.open span:nth-child(3) {
+  transform: rotate(-45deg);
+}
+
+.menu.open span:nth-child(4) {
+  top: 1.5vh;
+  width: 0;
+  left: 50%;
+}
+
 main {
   background: var(--color-champagne);
 }
@@ -199,10 +256,6 @@ main {
 .logo {
   width: 45vw;
   height: 45vw;
-}
-
-.menu {
-  align-self: flex-end;
 }
 
 .content {
